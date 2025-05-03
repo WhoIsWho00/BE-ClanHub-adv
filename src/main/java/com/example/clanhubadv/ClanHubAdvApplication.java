db59f8d222b43c9bb9cca99308d19f7057da0a36
WhoIsWho <teplowam@ukr.net>
2025-05-03 19:10:21 +0200

    test commit

---
+++ src/main/java/com/example/clanhubadv/ClanHubAdvApplication.java
@@ -0,0 +1,13 @@
+package com.example.clanhubadv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClanHubAdvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClanHubAdvApplication.class, args);
+    }
+
+}
